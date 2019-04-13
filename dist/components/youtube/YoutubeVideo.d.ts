8115753f96dc74115f94a7a8e966530008fc898e
Matthias Schulz <matthias.schulz@jash.de>
2019-04-13 15:06:33 +0200

    Added: YoutubeVideo – an easy way to playback and control videos from Youtube

---
+++ dist/components/youtube/YoutubeVideo.d.ts
@@ -0,0 +1,51 @@
+import { UIComponent } from "@webfruits/core";
+import { Signal } from "@webfruits/core/dist/signal/Signal";
+/******************************************************************
+ * YoutubeVideo
+ *
+ * https://developers.google.com/youtube/iframe_api_reference
+ *
+ * @author matthias.schulz@jash.de
+ *****************************************************************/
+export declare class YoutubeVideo extends UIComponent {
+    private _youtubeID;
+    /******************************************************************
+     * Properties
+     *****************************************************************/
+    static readonly DEFAULT_PLAYER_VARS: {};
+    private _ytpAPI;
+    private _playerVars;
+    private _iframeContainer;
+    onVideoCompletedSignal: Signal<void>;
+    onVideoPlayingSignal: Signal<void>;
+    onVideoPausedSignal: Signal<void>;
+    onYTPAPIAvailableSignal: Signal<void>;
+    onYTPAPIDestroyedSignal: Signal<void>;
+    /******************************************************************
+     * Constructor
+     *****************************************************************/
+    constructor(_youtubeID: string, playerVars?: {});
+    /******************************************************************
+     * Public Methodes
+     *****************************************************************/
+    readonly ytpAPI: any;
+    destory(): void;
+    updateStyles(): void;
+    play(): void;
+    stop(): void;
+    /******************************************************************
+     * Private Methodes
+     *****************************************************************/
+    private initIFrameContainer;
+    private loadYouTubeAPI;
+    private initYTElement;
+    private initYTPlayer;
+    private playVideo;
+    private stopVideo;
+    private destroyYTPlayer;
+    /******************************************************************
+     * Events
+     *****************************************************************/
+    private onPlayerReady;
+    private onPlayerStateChanged;
+}
