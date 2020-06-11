bae740697869b77f6ca99804f6c7f8d4a504c1c3
Matthias Schulz <matthias.schulz@jash.de>
2020-06-11 14:12:00 +0200

    Added: `StateValue` to easily create states objects where you can listen to

---
+++ dist/state/StateValue.js
@@ -0,0 +1,42 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Signal_1 = require("@webfruits/core/dist/signal/Signal");
+/******************************************************************
+ * StateValue
+ *
+ * @author matthias.schulz@jash.de
+ *****************************************************************/
+var StateValue = /** @class */ (function () {
+    /******************************************************************
+     * Constructor
+     *****************************************************************/
+    function StateValue(_defaultValue) {
+        if (_defaultValue === void 0) { _defaultValue = undefined; }
+        this._defaultValue = _defaultValue;
+        this.onChangeSignal = new Signal_1.Signal();
+        this._value = this._defaultValue;
+    }
+    /******************************************************************
+     * Public Methodes
+     *****************************************************************/
+    StateValue.prototype.getValue = function () {
+        return this._value;
+    };
+    StateValue.prototype.setValue = function (value, skipChangeSignal) {
+        if (skipChangeSignal === void 0) { skipChangeSignal = false; }
+        if (value === this._value)
+            return;
+        this._value = value;
+        if (skipChangeSignal)
+            return;
+        this.onChangeSignal.dispatch();
+    };
+    StateValue.prototype.hasValue = function () {
+        return !(this._value === null || this._value === undefined);
+    };
+    StateValue.prototype.reset = function () {
+        this.setValue(this._defaultValue);
+    };
+    return StateValue;
+}());
+exports.StateValue = StateValue;
---
+++ src/state/StateValue.ts
@@ -0,0 +1,61 @@
+import {Signal} from "@webfruits/core/dist/signal/Signal";
+
+/******************************************************************
+ * StateValue
+ *
+ * @author matthias.schulz@jash.de
+ *****************************************************************/
+
+export class StateValue<T> {
+
+    /******************************************************************
+     * Properties
+     *****************************************************************/
+
+    private _value: T;
+    public onChangeSignal = new Signal();
+
+    /******************************************************************
+     * Constructor
+     *****************************************************************/
+
+    constructor(private _defaultValue: T = undefined) {
+        this._value = this._defaultValue;
+    }
+
+    /******************************************************************
+     * Public Methodes
+     *****************************************************************/
+
+    public getValue(): T {
+        return this._value;
+    }
+
+    public setValue(value: T, skipChangeSignal: boolean = false) {
+        if (value === this._value) return;
+        this._value = value;
+        if (skipChangeSignal) return;
+        this.onChangeSignal.dispatch();
+    }
+
+    public hasValue(): boolean {
+        return !(this._value === null || this._value === undefined);
+    }
+
+    public reset() {
+        this.setValue(this._defaultValue);
+    }
+
+    /******************************************************************
+     * Private Methodes
+     *****************************************************************/
+
+    // no private methodes yet
+
+    /******************************************************************
+     * Events
+     *****************************************************************/
+
+    // no events yet
+
+}
